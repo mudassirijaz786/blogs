f02581e7dbfeea6451d8573fe961f438ceb7d814
mudassir-ijaz <ijazmudassir786@gmail.com>
2019-08-06 13:51:34 +0500

    just footer remaining

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
   before_action :authenticate_user!
   def index
     @articles = Article.all
